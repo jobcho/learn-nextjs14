185b6404112c36e325329fdf4f5639223a836cf2
jobcho <jobchojiyo@gmail.com>
2024-05-21 11:26:39 +0900

    Add files via upload

---
+++ app/(movies)/movies/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function LoadingMovie() {
+  return <h2>Loading a movie...</h2>;
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Not found",
+};
+export default function NotFount() {
+  return <h1>Not Found!</h1>;
+}
